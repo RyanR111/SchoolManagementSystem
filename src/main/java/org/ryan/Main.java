bbaac03649ece21553371be034952f80f414aae7
RyanR111 <6234930@edu.vaniercollege.qc.ca>
2023-12-03 22:28:30 -0500

    Creating objects in main

--- src/main/java/org/ryan/Main.java
+++ src/main/java/org/ryan/Main.java
@@ -1,7 +1,15 @@
 package org.ryan;
 
+import org.ryan.dto.Course;
+import org.ryan.dto.Department;
+import org.ryan.dto.Student;
+import org.ryan.dto.Teacher;
+
 public class Main {
     public static void main(String[] args) {
-
+        Department department = new Department();
+        Student student = new Student();
+        Teacher teacher = new Teacher();
+        Course course = new Course();
     }
 }
